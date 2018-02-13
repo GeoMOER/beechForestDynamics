83c1e1cad0e7a78888f7cb1e860af73004a25b5a
cwuestenhagen <33098140+cwuestenhagen@users.noreply.github.com>
2018-02-13 16:46:16 +0100

    final

--- beechForestDynamics/R/outlier_check_zum_testen.R
+++ beechForestDynamics/R/outlier_check_zum_testen.R
@@ -1,3 +1,5 @@
+###Zum Testen##
+
 
 #' Additional outlier check
 #' @title Additional outlier check
@@ -11,8 +13,20 @@
 #' @return geo-tiff data
 
 
-outlier_check <- function(rstack, outfilepathes){
+library(raster)
+library(doParallel)
+#devtools::install_github("environmentalinformatics-marburg/GSODTools")
+library(GSODTools)
+rstack <- raster::stack(list.files("C:/Users/coral/Downloads/modis/modis/test/", pattern=glob2rx("*.tif"), full.names=TRUE))
+outfilepathes <-"C:/Users/coral/Downloads/modis/modis/test/"
 
+lower_quantile = .4
+upper_quantile = .9
+
+
+
+outlier_check <- function(rstack, outfilepathes){
+  
   #take the matix, go through all rows and calculate the outliers
   #based on the lower and upper quantile
   rstack_matrix = raster::as.matrix(rstack)
@@ -29,17 +43,17 @@ outlier_check <- function(rstack, outfilepathes){
               }
               return(matrix(val, ncol = length(val), byrow = TRUE))
             }
-
+  
   #bind rows of the outliers together
   rstack_matrix_sd = do.call("rbind", rstack_matrix_sd)
   gc()
-
+  
   #assign outliers to layer
   for(l in seq(nlayers(rstack))){
     rstack[[l]] = raster::setValues(rstack[[l]], rstack_matrix[, l])
-
+    
     writeRaster(rstack, format="GTiff",
-                filename= outfilepathes, overwrite=TRUE)
+                filename= paste0(outfilepathes, "test.tif"), overwrite=TRUE)
   }
 }
 
