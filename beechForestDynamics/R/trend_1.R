9cb72bb2394495577d242c5b592659f52e7ff25b
MLindackers <33057672+MLindackers@users.noreply.github.com>
2018-02-13 12:30:20 +0100

    verbesserungsvorschläge

--- beechForestDynamics/R/trend_1.R
+++ beechForestDynamics/R/trend_1.R
@@ -3,10 +3,9 @@
 #' @details rank correlation coefficient Tau
 #' @usage significantTau(x, p, prewhitening, method, df, filename)
 
-#' @param p, numeric, defaults to 0.001. Significance level to be tested
-#' @param prewhitening, logical, If TRUE (default), pre-whitening is applied prior to the Mann-Kendall trend test.
-#' @param method, character, The prewhitening method to apply,
-#' @param df, logical, defaults to FALSE. If TRUE, a data.frame holding the value of Kendall's ?? and the referring significance level.
+#' @param p numeric, defaults to 0.001. Significance level to be tested
+#' @param prewhitening logical, If TRUE (default), pre-whitening is applied prior to the Mann-Kendall trend test.
+#' @param method character, The prewhitening method to apply,
 #' @param file_in list of all filenames
 #' @param file_fls_stau blablabla
 #' @return geo-tiff data
@@ -19,7 +18,7 @@
 file_in <- list.files("C:/Users/Marina/Documents/Uni/WiSe17-18/Umweltinformationssysteme/modis/modis_deseason_tiles/c0001-0511_r0001-0522/")
 file_fls_stau <- ("C:/Users/Marina/Documents/Uni/WiSe17-18/Umweltinformationssysteme/modis/modis_deseason_tiles/c0001-0511_r0001-0522/test.tif")
 
-trend <- function(p, prewhitening, method, df, file_in, file_fls_stau){
+trend <- function(p, prewhitening, method, file_in, file_fls_stau){
 
    #compute significantTau for significance levels
     rst_stau = gimms::significantTau(file_in=file_in,
