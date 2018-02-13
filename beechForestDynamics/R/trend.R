e75c2dcdbf7f23da25507c450799c54dcd69f34c
Kleebaue <kleebaue@students.uni-marburg.de>
2018-02-13 15:03:00 +0100

    next Version

--- beechForestDynamics/R/trend.R
+++ beechForestDynamics/R/trend.R
@@ -2,7 +2,7 @@
 #' @title trend
 #' @details rank correlation coefficient Tau
 #' @usage significantTau(x, p, prewhitening, method, filename)
-#' @param file_in list of input filenames
+#' @param file_in raster or rasterstack
 #' @param p, numeric, defaults to 0.001. Significance level to be tested
 #' @param prewhitening,logical, If TRUE (default), pre-whitening is applied prior to the Mann-Kendall trend test.
 #' @param method, character, The prewhitening method to apply,
