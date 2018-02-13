cce55e4acf9c5657159aec5c11abed506686ced0
Kleebaue <kleebaue@students.uni-marburg.de>
2018-02-13 15:14:53 +0100

    Version 1.4

--- beechForestDynamics/R/trend.R
+++ beechForestDynamics/R/trend.R
@@ -1,13 +1,14 @@
 #' trend
 #' @title trend
 #' @details rank correlation coefficient Tau
-#' @usage significantTau(x, p, prewhitening, method, filename)
+#' @usage trend(file_in, p, prewhitening, method, filename)
 #' @param file_in raster or rasterstack
 #' @param p, numeric, defaults to 0.001. Significance level to be tested
 #' @param prewhitening,logical, If TRUE (default), pre-whitening is applied prior to the Mann-Kendall trend test.
 #' @param method The prewhitening method to apply ("yuepilon", "zhang")
 #' @param filename output filename
 #' @return rst_stau; geo-tiff data
+#' @author HammerLe, Kleebaue
 
 trend <- function(file_in, p, prewhitening, method, filename){
 
@@ -15,8 +16,7 @@ trend <- function(file_in, p, prewhitening, method, filename){
     rst_stau = gimms::significantTau(x = file_in,
                                      p = p,
                                      prewhitening = prewhitening,
-                                     method = method,
-                                     filename = filename)
+                                     method = method)
 
     if (!dir.exists(filename[1]))
       dir.create(filename[1], recursive = TRUE)
