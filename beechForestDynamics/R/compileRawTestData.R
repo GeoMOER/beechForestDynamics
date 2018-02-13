8f158a3e6f9210104f6ebb085deaf936862591dd
tnauss <nauss@staff.uni-marburg.de>
2018-02-13 20:02:24 +0100

    Update rawtestData

--- beechForestDynamics/R/compileRawTestData.R
+++ beechForestDynamics/R/compileRawTestData.R
@@ -10,26 +10,23 @@
 
 compileRawTestData = function(){
 
-raster::writeRaster(rstck, filename = outfiles,
-                                      format = "GTiff", bylayer = TRUE, overwrite = TRUE)
-inpath = "C:/Users/tnauss/permanent/edu/msc-phygeo-environmental-observations/data/modis"
-outpath = paste0(dirname(inpath), "/testdata_bFD")
-dirs = list.dirs(path)[-1]
-dirs = dirs[-grep("mktrends", dirs)]
+  inpath = "F:/modis_carpathian_mountains/modis/tiles/c0001-0511_r0001-0522/"
+  outpath = "F:/modis_carpathian_mountains/data/data_small/modis/tiles/c0001-0511_r0001-0522/"
+  dirs = list.dirs(inpath)[-1]
 
   for(dir in dirs){
-      files = list.files(dir, pattern = glob2rx("*.tif"), full.names = TRUE)
-      if(length(files) > 0){
-          files = files[1:3]
-          rstck = raster::stack(files)
-          rstck = raster::crop(rstck, raster::extent(rstck, 1, 15, 1, 15))
-          outfilepath = paste0(outpath,
-                                                          substr(dir, attr(regexpr(inpath, dir),"match.length")+1, nchar(dir)))
-          if (!dir.exists(outfilepath))
-              dir.create(outfilepath, recursive = TRUE)
-          outfiles = paste0(outfilepath, "/", basename(files))
-          raster::writeRaster(rstck, filename = outfiles,
-                                                format = "GTiff", bylayer = TRUE, overwrite = TRUE)
-        }
+    files = list.files(dir, pattern = glob2rx("*.tif"), full.names = TRUE)
+    lf = length(files)
+    if(lf > 0){
+      rstck = raster::stack(files)
+      rstck = raster::crop(rstck, raster::extent(rstck, 1, 15, 1, 15))
+      outfilepath = paste0(outpath, substr(dir, attr(regexpr(inpath, dir),"match.length")+1, nchar(dir)))
+
+      if (!dir.exists(outfilepath))
+        dir.create(outfilepath, recursive = TRUE)
+
+      outfiles = paste0(outfilepath, "/", basename(files))
+      raster::writeRaster(rstck, filename = outfiles, format = "GTiff", bylayer = TRUE, overwrite = TRUE)
     }
+  }
 }
