c9e8939db4768aee0d5804336a88d82afb623558
MLindackers <33057672+MLindackers@users.noreply.github.com>
2018-02-16 17:32:25 +0100

    update spatialProjection

--- R/spatialProjection.R
+++ R/spatialProjection.R
@@ -3,9 +3,10 @@
 #' @details  Projects a tif files into another projection from a given tif file.
 #' @usage spatialProjection=function(rst_from,prst_to,outfilepath)
 #' dependent on package "raster" and "rgdal"
-#' @param rst_from List of input tif files
-#' @param prst_to List of designated projected tif files
+#' @param list_rst_from charachter string: List of input tif files
+#' @param list_prst_to charachter string: List of designated projected tif files
 #' @param outfilepath path where output tif files shall be stored
+#' @param areaname three letters shortcut for study area
 #' @return geo tif data 
 #' @export spatialProjection
 #' @aliases spatialProjection
@@ -14,16 +15,21 @@
 #' spatialProjection=function(rst_from,prst_to,outfilepath)
 #' }
 #'
-spatialProjectionion=function(rst_from,prst_to,outfilepath){
-  for (i in seq(1:length(rst_from))){
-    mwesp_stck= raster::stack(rst_from[which(i==substr(basename(path = rst_from),10,13))])#aktuell 16,19
-    for (j in seq(1:length(prst_to)))
-      
-      proj_stck= raster::stack(prst_to[which(i==substr(basename(path = prst_to),10,13))])
+
+
+###function
+spatialProjection = function(list_rst_from, list_prst_to, outfilepath, areaname=NULL, a=11, b=14, c=11, d=14){
+  years=unique(as.numeric(substr(basename(path = list_rst_from), a, b)))
+  
+  for (i in years){
+    mswep_stack=raster::stack(list_rst_from[which(i==substr(basename(path = list_rst_from), c, d))])
     
-    projectRaster(from=mwesp_stck, to=proj_stck,
+    proj_tile=raster(list_prst_to[1])
+    
+    projectRaster(from=mswep_stack, to=proj_tile,
                   method="bilinear", 
-                  filename=outputfilepath,format="GTiff", overwrite = T) 
+                  filename=paste0(outfilepath, "MSWEP_",areaname, i,"_projected"),
+                  format="GTiff", overwrite = T, bylayer=F) 
     
   }
 }
