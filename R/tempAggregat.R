324ffa3661153eecf53dede950237c55a07a0553
Marina Lindackers <Lindacke@STUDENTS>
2018-02-16 16:16:11 +0100

    completly variable

--- R/tempAggregat.R
+++ R/tempAggregat.R
@@ -1,30 +1,34 @@
-#' @title tempAggregat
+#' @title flexTimeAggStack
 #' @details This function aggregates, matching to destination dates, tiff layers and sums them to a single layer.
 #' dependent on package "raster" & "rgdal"
-#' @param beginzeitsp, integer, year of start 
+#' @param beginzeitsp, integer, year of start
 #' @param endzeitsp, integer, year of end
 #' @param dates_path, filepath to sorted and listet data with date info
 #' @param aggrdata, filepath to stored data which shall be temporally aggregated
 #' @param edit, character, shortnote for data [MAX 3 LETTERS]
 #' @param outfilepath, path to store temporal aggregated tif files
+#' @param a integer, first pos of annual date
+#' @param b integer, last pos of annual date
+#' @param c integer, first pos of daily date
+#' @param d integer, last pos of daily date
 #'
 #' @return geo-tiff data
 #'
-#' @export tempAggregat
+#' @export flexTimeAggStack
 #' @author A. Santowski & L. Giese
 #' #'@examples
 #' \dontrun{
-#' tempAggregat=function(beginnzeitsp,endzeitsp,dates_path,aggrdata,outfilepath,edit="kap")
+#' flexTimeAggStack=function(beginnzeitsp,endzeitsp,dates_path,aggrdata,outfilepath,edit="kap")
 #' }
-tempAggregat=function(beginnzeitsp,endzeitsp,dates_path,aggrdata,outfilepath,edit="kap"){
-  
+flexTimeAggStack=function(beginnzeitsp,endzeitsp,dates_path,aggrdata,outfilepath,edit="kap",a=1,b=4,c=5,d=7){
+
   for(j in seq(beginnzeitsp,endzeitsp)){
     print(j)
-    
-    lc = substr(dates[substr(dates, 1, 4) == j], 5, 7)
+
+    lc = substr(dates[substr(dates, a, b) == j], c, d)
     l = as.numeric(lc)
     stacklist=raster::stack(aggrdata[which(j == seq(beginnzeitsp,endzeitsp))])
-    
+
     foreach(i=seq(1:(length(l))),
             .export=c("stacklist", "outfilepath", "edit"),
             .packages = c("raster", "rgdal")) %dopar%{
@@ -34,12 +38,12 @@ tempAggregat=function(beginnzeitsp,endzeitsp,dates_path,aggrdata,outfilepath,edi
               } else {
                 temp_agg=sum(stacklist[[l[i]:(l[i+1]-1)]])
               }
-              raster::writeRaster(temp_agg, 
+              raster::writeRaster(temp_agg,
                                   paste0(outfilepath, "MSWEP_",edit,"_", j, lc[i], "_temporal_aggregated"),
                                   format="GTiff",overwrite=T)
-              
+
             }
   }
-  
+
 }
 
